280c9ea4d7dc78bb18e6d0eb2d50c9ce4acc3a65
Yuancheng Zhang <old.far.hot@gmail.com>
2022-03-29 10:31:50 +0800

    remove mongodb

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 const Discord = require("discord.js");
-const mongoose = require("mongoose");
-const User = require("./User");
 
 const TOKEN = "OTU2ODYxMDM4MTAyNDcwNjU4.Yj2YSQ.L_rOW3g5feySOv5Y6tTDl5KhNiE";
 
@@ -8,9 +6,6 @@ const client = new Discord.Client({
   intents: ["GUILDS", "GUILD_MESSAGES"],
 });
 
-mongoose.connect(
-  "mongodb+srv://en:mciGODtvNITv2yZg@cluster0.sqg2x.mongodb.net/Discord?retryWrites=true&w=majority"
-);
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
@@ -22,22 +17,6 @@ client.on("messageCreate", (message) => {
   }
 });
 
+
+
 client.login(TOKEN);
-
-// en
-// mciGODtvNITv2yZg
-
-async function run() {
-  const user = await User.create({
-    name: "Kylge",
-    age: 26,
-    hobbies: ["Weight Lifting", "Bowling"],
-    address: {
-      steet
-    }
-  });
-  user.name = "Sally";
-  console.log(user);
-}
-
-run();
